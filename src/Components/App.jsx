f5e32d723b817a2391e1ea2c3d46133bc898836c
hr21don <h_R@helitharupasinghe.com>
2021-11-26 15:44:04 +0000

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import Notes from "../notes";
+
+function App() {
+  return (
+    // * => **
+    <div>
+      <Header />
+      {Notes.map((single_note) => (
+        <Note
+          key={single_note.key}
+          title={single_note.title}
+          content={single_note.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+export default App;
