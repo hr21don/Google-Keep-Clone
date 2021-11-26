e2406daa79659aea4ae3f26d509400b508cc9c3b
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2021-11-26 16:27:14 +0000

    Update notes.js

--- src/notes.js
+++ src/notes.js
@@ -22,6 +22,36 @@ const notes = [
     title: "Hardware vs. Software",
     content:
       "What's the difference between hardware and software? You can hit your hardware with a hammer, but you can only curse at your software."
+  },
+  {
+    key: 5,
+    title: "Big ideas pt 1",
+    content: "Eat more sushi"
+  },
+  {
+    key: 6,
+    title: "Big ideas pt 2",
+    content: "Eat more Fish"
+  },
+  {
+    key: 7,
+    title: "Big ideas pt 3",
+    content: "Eat more Chicken"
+  },
+  {
+    key: 8,
+    title: "Big ideas pt 4",
+    content: "Eat more Rice"
+  },
+  {
+    key: 9,
+    title: "Big ideas pt 5",
+    content: "Eat more Salad"
+  },
+  {
+    key: 10,
+    title: "Big ideas pt 6",
+    content: "Eat more Fruit"
   }
 ];
 export default notes;
